cdc31bed571c79d1fc076ba462857ba28b2023ea
hasnidajaafar <g-02074128@moe-dl.edu.my>
2024-06-23 15:01:49 +0800

    latihan
    bengkel

---
+++ aktiviti3.4.py
@@ -0,0 +1,26 @@
+print("Jawab soalan-soalan matematik berikut")
+
+#pemboleh ubah
+soalan1="Ya"
+soalan2=0.375
+soalan3="Betul"
+
+#output soalan & input jawapan bagi setiap soalan
+print("SOALAN 1")
+print("Tentukan sama ada 6 ialah afktor bai 96. Jawab Ya atau Tidak.")
+jawapan1=str(input("jawapan :" ))
+print(jawapan1==soalan1)
+print()
+
+print("SOALAN 2")
+print("Hitungkan 1-3x5/24. Beri jawapan dalam 3 titik perpuluhan")
+jawapan2=float(input("jawapan :" ))
+print(jawapan2==soalan2)
+print()
+
+print("SOALAN 3")
+print("Adakah ungkapan berikut 'Betul' atau 'Salah'.")
+print(6+(-7)<3-(-4))
+jawapan3=str(input("jawapan :" ))
+print(jawapan3==soalan3)
+print()
